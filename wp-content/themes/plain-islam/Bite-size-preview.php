b4c4a784270270fffd52bead67c6f5fae588d486
Louis Smith <louis@ahoia.com>
2015-04-09 17:22:54 +0100

    Added animation to bite-size

---
+++ wp-content/themes/plain-islam/Bite-size-preview.php
@@ -0,0 +1,149 @@
+<?php /* Template Name: bite-size preview layout */ get_header(); ?>
+
+<!-- 	<div class="header-hero-image top-section" >
+		<h1><?php the_title(); ?></h1>
+	</div> -->
+
+	<main role="main">
+	
+			
+		<section id="bite-size">
+			<div class="wrapper">
+				<h2>Bite-size</h2>
+				<p>This website aims to provide information on Islam and Muslims to non-specialist audiences in a way that is clear, simple and accessible. Begin here by clicking on any topic.</p>				
+				<ul>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title">
+							helllooooo…
+						</div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title">
+							
+						</div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+					<li>
+						<img src="" alt="">
+						<div class="bite-size__title"></div>
+					</li>
+				</ul>
+			</div>
+
+		</section><!-- //bite-size -->
+	</main>
+
+<?php get_footer(); ?>
