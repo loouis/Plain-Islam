fe0f1a0d22d8659c98ef20b2a50821a4ed96679c
Louis Smith <louis@ahoia.com>
2015-04-10 17:38:30 +0100

    Add video lightbox and vertically center wrapper

--- wp-content/themes/plain-islam/js/scripts.js
+++ wp-content/themes/plain-islam/js/scripts.js
@@ -1,37 +1,55 @@
-(function ($, root, undefined) {
+
+jQuery(document).ready(function($){
 	
-	$(function () {
-		
-		'use strict';
-		
-		//View more bite-size on the homepage
-		$('.bite-size__view-more-button').click(function(e){
-			e.preventDefault();
-			$('section#bite-size').addClass('auto-height');
-			$('.load-more-container, .bite-size__view-more-button').fadeOut();
-		});
-
-		//Init bxslider
-		$('ul.bx-slider').bxSlider({
-			auto: true,
-			mode: 'horizontal',
-			pause: 7000,
-
-			// nextSelector: '#slider-next',
-			// prevSelector: '#slider-prev',
-			// nextText: 'Onward →',
-			// prevText: '← Go back'
-		});
-
-		//Open lightbox
-
-		//close contact lightbox
-		$('button.close').click(function(){
-			$('section#lightbox').fadeOut();
-		});
-
-		
-		
+	// 'use strict';
+	
+	//View more bite-size on the homepage
+	$('.bite-size__view-more-button').click(function(e){
+		e.preventDefault();
+		$('section#bite-size').addClass('auto-height');
+		$('.load-more-container, .bite-size__view-more-button').fadeOut();
 	});
+
+	//Init bxslider
+	$('ul.bx-slider').bxSlider({
+		auto: true,
+		mode: 'horizontal',
+		pause: 7000,
+	});
+
+	//homepage video lightbox
+	$('#homepage-video-button').click(function(e){
+		e.preventDefault();
+		$('body').addClass("no-scroll");
+		$('#video-lightbox').fadeIn();
+	});
+
+	//Open lightbox
+
+
+	//close contact lightbox
+	$('button.close').click(function(){
+		$('body').removeClass("no-scroll");
+		$('section#lightbox, section#video-lightbox').fadeOut();
+	});
+
+	 //center video in lightbox maths and style
+	function centerLightbox (){
+		//Variables for lightbox
+		var windowHeight = $(window).height();
+		var lightboxWrapper = $('.lightbox .wrapper');
+		var lightboxWrapperHeight = lightboxWrapper.height();
+
+		//Margintop: lightbox height - video height / 2
+		lightboxWrapper.css('margin-top', - (lightboxWrapperHeight - windowHeight) / 2);
+	}
+	centerLightbox();
+
+	$(window).resize(function(){
+		centerLightbox();
+	});
+
 	
-})(jQuery, this);
+	
+})(jQuery);
+
