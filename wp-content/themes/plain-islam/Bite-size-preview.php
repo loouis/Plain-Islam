7365a4c59d4e2e6138964f44e73f8affc482cd4e
Louis Smith <louis@ahoia.com>
2015-04-14 09:49:03 +0100

    Bite-size page added

--- wp-content/themes/plain-islam/Bite-size-preview.php
+++ wp-content/themes/plain-islam/Bite-size-preview.php
@@ -13,132 +13,128 @@
 				<p>This website aims to provide information on Islam and Muslims to non-specialist audiences in a way that is clear, simple and accessible. Begin here by clicking on any topic.</p>				
 				<ul>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title">
-							helllooooo…
-						</div>
+						<img src="<?php bloginfo(template_url);?>/img/abraham.jpg" alt="">
+						<div class="bite-size__title">Abraham</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/god.jpg" alt="">
+						<div class="bite-size__title">God</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/eternal-message.jpg" alt="">
+						<div class="bite-size__title">Eternal message</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/charity.jpg" alt="">
+						<div class="bite-size__title">Charity</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/citizenship.jpg" alt="">
+						<div class="bite-size__title">Citizenship</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/humanity.jpg" alt="">
+						<div class="bite-size__title">Humanity</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/democracy.jpg" alt="">
+						<div class="bite-size__title">Democracy</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/eco-islam.jpg" alt="">
+						<div class="bite-size__title">Eco-Islam</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/entertainment.jpg" alt="">
+						<div class="bite-size__title">Entertainment</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/equality.jpg" alt="">
+						<div class="bite-size__title">Equality</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/heroes.jpg" alt="">
+						<div class="bite-size__title">Heroes</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/freedom.jpg" alt="">
+						<div class="bite-size__title">Freedom</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/mercy.jpg" alt="">
+						<div class="bite-size__title">Mercy</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/character.jpg" alt="">
+						<div class="bite-size__title">Character</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/human-nature.jpg" alt="">
+						<div class="bite-size__title">Human nature</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/human-rights.jpg" alt="">
+						<div class="bite-size__title">Human rights</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/intergration.jpg" alt="">
+						<div class="bite-size__title">Intergration</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/modern-life.jpg" alt="">
+						<div class="bite-size__title">Modern life</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/jesus.jpg" alt="">
+						<div class="bite-size__title">Jesus</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/jihad.jpg" alt="">
+						<div class="bite-size__title">Jihad</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/living-with-difference.jpg" alt="">
+						<div class="bite-size__title">Living with difference</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/peace.jpg" alt="">
+						<div class="bite-size__title">Peace</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/muhammad.jpg" alt="">
+						<div class="bite-size__title">Muhammad</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/prophets.jpg" alt="">
+						<div class="bite-size__title">Prophets</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/quran.jpg" alt="">
+						<div class="bite-size__title">Quran</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/responsibility.jpg" alt="">
+						<div class="bite-size__title">Responsiblity</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title">
-							
-						</div>
+						<img src="<?php bloginfo(template_url);?>/img/sectarian.jpg" alt="">
+						<div class="bite-size__title">Sectarian</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/sharia.jpg" alt="">
+						<div class="bite-size__title">Sharia</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/science.jpg" alt="">
+						<div class="bite-size__title">Science</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/belief.jpg" alt="">
+						<div class="bite-size__title">Belief</div>
 					</li>
 					<li>
-						<img src="" alt="">
-						<div class="bite-size__title"></div>
+						<img src="<?php bloginfo(template_url);?>/img/worship.jpg" alt="">
+						<div class="bite-size__title">Worship</div>
 					</li>
 				</ul>
 			</div>
